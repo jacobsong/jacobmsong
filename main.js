72b20d9447d8b9fe720111afe3f4436544244dde
Jacob Song <jsong@ACHRCWK0900L452.usarec.army.mil>
2018-07-06 16:19:44 -0400

    menu stuff

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
   const sideNav = document.querySelector(".sidenav");
   M.Sidenav.init(sideNav, {});
+
+  showPage("aboutmebtn", "aboutme");
 });
 
-function showPage(elem, selectedPage) {
+function showPage(selectedElem, selectedPage) {
   const pages = ["aboutme", "resume", "portfolio", "contact"];
+  const elems = ["aboutmebtn", "resumebtn", "portfoliobtn", "contactbtn"];
+
   for (let page of pages) {
     if (selectedPage === page) {
       document.getElementById(page).style.display = "block";
@@ -12,5 +16,12 @@ function showPage(elem, selectedPage) {
       document.getElementById(page).style.display = "none";
     }
   }
-  elem.parentElement.classList.add("active");
+
+  for (let elem of elems) {
+    if (selectedElem === elem) {
+      document.getElementById(elem).classList.add("sidenav-clicked");
+    } else {
+      document.getElementById(elem).classList.remove("sidenav-clicked");
+    }
+  }
 }
