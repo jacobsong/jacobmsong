75dbb429dc5776a20f726c654b43723093e923d9
Jacob Song <jacobsong@Jacobs-MacBook-Pro.local>
2018-07-08 22:13:47 -0400

    fix server

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const path = require("path");
 const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
 
 const PORT = process.env.PORT || 5000;
 
